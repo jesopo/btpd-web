27b1957f7eafea647f624694df164b62132e7263
jesopo <github@lolnerd.net>
2015-11-29 15:06:11 +0000

    changed order of columns in output and added sorting logic
    Signed-off-by: jesopo <github@lolnerd.net>

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ app.config.from_object(Config)
 @app.route("/")
 def index():
 	orderby = flask.request.args.get("orderby")
-	reverse = False
+	descending = True
 	if orderby and orderby.startswith("-"):
-		reverse = True
+		descending = False
 		orderby = orderby[1:]
-	lines = subprocess.check_output(["btcli", "list", "-f", "%n %# %t %p %s %r\\n"]
+	lines = subprocess.check_output(["btcli", "list", "-f", "%n %# %t %p %S %r %s\\n"]
 		).decode("utf8").strip().split("\n")
 	if not orderby or not orderby.isdigit() or int(orderby) > len(lines)-1:
 		orderby = 0
@@ -29,14 +29,24 @@ def index():
 		failed = True
 	else:
 		for line in lines[1:]:
-			line = line.rsplit(None, 5)
+			line = line.rsplit(None, 6)
+			line.insert(0, int(line.pop(1)))
+			line.insert(3, float(line.pop(3)[:-1]))
+			line.insert(4, int(line.pop(4)))
+			line.insert(5, float(line.pop(5)))
 			if line[2] in TORRENT_STATES:
 				line[2] = TORRENT_STATES[line[2]]
 			parsed_lines.append(line)
-	orders = ["%s%d" % ("-" if n == orderby and not reverse else "", n) for n in range(6)]
-	parsed_lines = sorted(parsed_lines, key=lambda l: l[orderby], reverse=reverse)
+	orders = ["%s%d" % ("-" if n == orderby and descending else "", n) for n in range(6)]
+	parsed_lines = sorted(parsed_lines, key=lambda l: l[orderby], reverse=descending)
+	for n, line in enumerate(parsed_lines):
+		line[3] = "%.1f" % line[3]
+		line[4] = line.pop(6)
+		line[5] = "%.2f" % line[5]
+		parsed_lines[n] = line
 	return flask.render_template("index.html", failed=failed,
 		lines=parsed_lines, orders=orders)
+
 @app.route("/action")
 def torrent_action():
 	print(flask.request.referrer)
@@ -55,6 +65,7 @@ def torrent_action():
 	if not flask.request.is_xhr:
 		return flask.redirect("%s%s" % (flask.url_for("index"), referrer_params))
 	return "0" if out else "1"
+
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", debug=True)
 
