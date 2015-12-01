009ffac40dbb4ee4b453e6b6ce95353109611b87
jesopo <github@lolnerd.net>
2015-12-01 20:04:52 +0000

    changed index.html to be a frame with a 'fragment' in it, added up/down arrows when ordering by a heading.
    Signed-off-by: jesopo <github@lolnerd.net>

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import Config
 TORRENT_STATES = {"S": "seed", "I": "idle", "L": "leech", "+": "starting"}
 TORRENT_ACTIONS = {"seed": "stop", "idle": "start", "leech": "start"}
 
+HEADINGS= ["ID", "Name", "Status", "Percent", "Size", "Ratio"]
+ARROW_DOWN = "▾"
+ARROW_UP = "▴"
+
 app = flask.Flask(__name__)
 app.config.from_object(Config)
 
@@ -14,15 +18,18 @@ app.config.from_object(Config)
 def index():
 	orderby = flask.request.args.get("orderby")
 	descending = True
+	headings = HEADINGS[:]
 	if orderby and orderby.startswith("-"):
 		descending = False
 		orderby = orderby[1:]
 	lines = subprocess.check_output(["btcli", "list", "-f", "%n %# %t %p %S %r %s\\n"]
 		).decode("utf8").strip().split("\n")
-	if not orderby or not orderby.isdigit() or int(orderby) > len(lines)-1:
+	if not orderby or not orderby.isdigit() or int(orderby) >= len(lines)-1:
 		orderby = 0
 	else:
 		orderby = int(orderby)
+		arrow = ARROW_DOWN if descending else ARROW_UP
+		headings[orderby] = "%s %s" % (headings[orderby], arrow)
 	parsed_lines = []
 	failed = False
 	if lines[0].startswith("cannot open connection"):
@@ -45,11 +52,11 @@ def index():
 		line[5] = "%.2f" % line[5]
 		parsed_lines[n] = line
 	return flask.render_template("index.html", failed=failed,
-		lines=parsed_lines, orders=orders)
+		lines=parsed_lines, orders=orders, fragment="list.html",
+		headings=headings)
 
 @app.route("/action")
 def torrent_action():
-	print(flask.request.referrer)
 	referrer_params = flask.request.referrer.split("?", 1)
 	if len(referrer_params) > 1:
 		referrer_params = "?%s" % referrer_params[1]
@@ -59,7 +66,6 @@ def torrent_action():
 	id = flask.request.args["id"]
 	if not id.isdigit() or not state in TORRENT_ACTIONS:
 		return flask.abort(400)
-	print(TORRENT_ACTIONS[state])
 	out = subprocess.check_output(["btcli", TORRENT_ACTIONS[state], id],
 		stderr=subprocess.STDOUT)
 	if not flask.request.is_xhr:
