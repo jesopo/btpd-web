463e6892fa109ca14173faf44b25d03a78fe36c8
jesopo <github@lolnerd.net>
2016-02-16 08:42:21 -0500

    started work on adding pleasant javascript keybindings, for keyboard navigation.
    Signed-off-by: jesopo <github@lolnerd.net>

--- static/script.js
+++ static/script.js
@@ -3,3 +3,14 @@ $(document).ready(function() {
 		$("#takefocus").focus();
 	}
 });
+
+$(document).keyup(function(e) {
+	var target = e.target.tagName.toLowerCase();
+	if (target == "body") {
+		var charHit = String.fromCharCode(e.which || e.keyCode
+			).toLowerCase();
+		if (charHit == 'n') {
+			window.location.href = "/add";
+		}
+	}
+});
