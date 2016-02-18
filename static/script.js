5041f39b46ae57c2465e21ab7d54adaa17295e4b
jesopo <github@lolnerd.net>
2016-02-18 08:14:50 -0500

    added javascript keyboard shortcuts for ordering by columns.
    Signed-off-by: jesopo <github@lolnerd.net>

--- static/script.js
+++ static/script.js
@@ -9,8 +9,19 @@ $(document).keyup(function(e) {
 	if (target == "body") {
 		var charHit = String.fromCharCode(e.which || e.keyCode
 			).toLowerCase();
-		if (charHit == 'n') {
-			window.location.href = "/add";
+		if ($.trim(charHit) != "") {
+			if (charHit == 'n') {
+				window.location.href = "/add";
+			} else if (charHit == 'h') {
+				window.location.href = "/";
+			} else if (!isNaN(charHit) && $(".torrentheadings").length) {
+				var headings = $(".torrents").find(".torrentheading")
+				var number = parseInt(charHit)-1;
+				if (number < headings.length) {
+					var heading = $(headings.get(number));
+					window.location.href  = heading.attr("href");
+				}
+			}
 		}
 	}
 });
